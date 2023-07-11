1750984fc0e26487b86811da9f70c0cb3cb01c9b
EvilRiser <shaurabh.saha@gmail.com>
2023-07-11 22:13:03 +0530

    struct upd

--- Day/recursion/11-May-2023/recursion.py
+++ Day/recursion/11-May-2023/recursion.py
@@ -1,7 +1,7 @@
 import sys
 
-sys.stdout = open("./output.txt","w")
-sys.stdin = open("./input.txt","r")
+sys.stdout = open("../output.txt","w")
+sys.stdin = open("../input.txt","r")
 
 
 a = input()
